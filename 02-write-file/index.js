d88c349e56363324819e14485f6e422b17866c97
Dalmarkov <139253030+dalmarkov@users.noreply.github.com>
2024-01-19 12:25:04 +0100

    feat: add writing console input to file

---
+++ 02-write-file/index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const filePath = path.join(__dirname, 'output.txt');
+
+function writeToFile(data) {
+  fs.appendFileSync(filePath, data + '\n');
+  console.log('Текст успешно добавлен в файл.');
+  promptUser();
+}
+
+function promptUser() {
+  rl.question(
+    'Введите текст (или нажмите Ctrl+C для завершения): ',
+    (userInput) => {
+      writeToFile(userInput);
+    },
+  );
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log('Привет! Введи текст для записи в файл.');
+
+promptUser();
+
+rl.on('close', () => {
+  console.log('Пока!');
+  process.exit(0);
+});
+
+rl.on('SIGINT', () => {
+  rl.close();
+});
