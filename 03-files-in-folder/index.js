5b7b814cabdbd7f79ac100ae80f7cb30ab778a44
Dalmarkov <139253030+dalmarkov@users.noreply.github.com>
2024-01-20 11:34:51 +0100

    feat: add code Displaying Information about Files Stored in a Folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+const requiredPath = path.resolve(__dirname, 'secret-folder');
+
+function getFileSize(filePath, callback) {
+  fs.stat(filePath, (err, stats) => {
+    if (err) {
+      console.error(err.message);
+      callback(err, null);
+    } else {
+      callback(null, stats.size);
+    }
+  });
+}
+
+fs.readdir(requiredPath, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    console.error('Error reading directory:', err);
+    return;
+  }
+
+  files.forEach((file) => {
+    if (!file.isDirectory()) {
+      const filePath = path.resolve(requiredPath, file.name);
+      const { name, ext } = path.parse(filePath);
+      const extPath = ext.substring(1);
+      getFileSize(filePath, (err, fileSize) => {
+        if (err) {
+          console.error(`Error processing file ${name}: ${err.message}`);
+        } else {
+          process.stdout.write(`${name} - ${extPath} - ${fileSize} bytes\n`);
+        }
+      });
+    }
+  });
+});
